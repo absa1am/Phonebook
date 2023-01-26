e4ac20babe91ef7c3b0a6b40268371ed866674c5
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-01-26 22:01:25 +0600

    Added DbContext Service

--- PhonebookAPI/Program.cs
+++ PhonebookAPI/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PhonebookAPI.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("PhonebookAPIDefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
