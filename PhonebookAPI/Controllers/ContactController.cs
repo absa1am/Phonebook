def147549f434a423722bd27440c496a5bf03b27
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-01-26 22:28:45 +0600

    Added GetContacts Method

--- PhonebookAPI/Controllers/ContactController.cs
+++ PhonebookAPI/Controllers/ContactController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PhonebookAPI.Contexts;
+using PhonebookAPI.Models;
 
 namespace PhonebookAPI.Controllers
 {
@@ -7,5 +9,22 @@ namespace PhonebookAPI.Controllers
     [ApiController]
     public class ContactController : ControllerBase
     {
+        private readonly AppDbContext appDbContext;
+
+        public ContactController(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ContactModel>>> GetContacts()
+        {
+            List<ContactModel> contacts = appDbContext.Contacts.ToList();
+
+            if (contacts == null || contacts.Count == 0)
+                return NotFound("Sorry, contact(s) not found.");
+
+            return Ok(contacts);
+        }
     }
 }
