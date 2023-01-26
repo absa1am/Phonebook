ffee2be1ba887b1a2fc35c7042ea184a9e3688ed
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-01-26 22:03:53 +0600

    Added Contact Controller

---
+++ PhonebookAPI/Controllers/ContactController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhonebookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+    }
+}
