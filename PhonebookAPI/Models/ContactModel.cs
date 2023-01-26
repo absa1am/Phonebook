dd690920660e5dd288c974fe2a807ff717545fcd
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-01-26 21:56:29 +0600

    Added ContactModel

---
+++ PhonebookAPI/Models/ContactModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhonebookAPI.Models
+{
+    public class ContactModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber), MinLength(11)]
+        public string Phone { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
