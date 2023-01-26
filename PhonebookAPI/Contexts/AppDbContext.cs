188c94d0f57910b6966583f88e279f666a0a1183
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-01-26 21:57:42 +0600

    Added DbContext

---
+++ PhonebookAPI/Contexts/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhonebookAPI.Models;
+
+namespace PhonebookAPI.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) { }
+
+        public DbSet<ContactModel> Contacts { get; set; }
+    }
+}
